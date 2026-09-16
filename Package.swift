b8292756ffc4e77da06b19b25f1665192d22e149
serverleader <serverleader@gmail.com>
2026-09-16 15:57:51 -0500

    deps: allow swift-crypto 4.x and 5.x (1.0.0..<6.0.0)
    Citadel cannot take swift-crypto 5 while this fork caps at <4.0.0.
    Do not change wire types or the 0.4.0 tag pin.

--- Package.swift
+++ Package.swift
@@ -46,7 +46,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"6.0.0"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.2"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
